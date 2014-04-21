d24a662d9d869a106e351d65cc2873d6cbb08db8
nicholasxjy <yitaqi2011@gmail.com>
2014-04-21 22:10:17 +0800

    some modify

--- public/javascripts/js/main.js
+++ public/javascripts/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-
+    //initial scrollreveal
+    window.scrollReveal = new scrollReveal();
+    // images display
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
     //add or cancel plus one
     $('.btn-plus-like').click(function() {
         var isPlus = (this.title === 'plus one');
